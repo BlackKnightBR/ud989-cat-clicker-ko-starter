ee540a09a9e376c2333f27ae01f71b64b5dc8680
Rorigo Montebello Saboya Brito <blackknight@blackknightstudio.com.br>
2017-05-03 17:42:52 -0300

    Criação do arquivo Javascript e adição de data-binds.

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var ViewModel = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.imgAttribution = ko.observable('Relax');
+
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+
+    var count = 0;
+    count++;
+  };
+}
+
+ko.applyBindings(new ViewModel());
